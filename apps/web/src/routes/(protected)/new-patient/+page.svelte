b6d89e596426abba80b154a90051e25686b4329a
joshuamotoaki <dachshunduniverse@gmail.com>
2024-11-09 18:42:29 -0500

    Begin creating patient form

--- apps/web/src/routes/(protected)/new-patient/+page.svelte
+++ apps/web/src/routes/(protected)/new-patient/+page.svelte
@@ -1,1 +1,31 @@
-<div></div>
+<script>
+    import { enhance } from "$app/forms";
+    import Button from "$lib/components/ui/button/button.svelte";
+    import Input from "$lib/components/ui/input/input.svelte";
+    import { Label } from "$lib/components/ui/label";
+
+    let name = "";
+
+    const handleSubmit = async () => {};
+</script>
+
+<main>
+    <div class="cont py-2">
+        <h2 class="text-2xl font-semibold text-blue-800">Add New Patient</h2>
+
+        <div>
+            <div class="mt-4 space-y-2">
+                <div class="grid w-full max-w-sm items-center gap-1.5">
+                    <Label for="name">Patient Name</Label>
+                    <Input
+                        bind:value={name}
+                        type="text"
+                        name="name"
+                        placeholder="Name" />
+                </div>
+            </div>
+
+            <Button onclick={handleSubmit} class="mt-4">Add Patient</Button>
+        </div>
+    </div>
+</main>
