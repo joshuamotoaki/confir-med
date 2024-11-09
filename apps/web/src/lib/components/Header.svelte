59c3f13050749f45cfb5de624540233f9ce7c8b7
joshuamotoaki <dachshunduniverse@gmail.com>
2024-11-09 17:37:08 -0500

    Create new patient form route

--- apps/web/src/lib/components/Header.svelte
+++ apps/web/src/lib/components/Header.svelte
@@ -35,9 +35,9 @@
                 }}>
                 <span> Logout </span>
             </Button>
-            <Button variant="outline">
+            <Button variant="outline" href="/new-patient">
                 <Plus />
-                <span> Add Patient </span>
+                <span> New Patient </span>
             </Button>
         </nav>
     </div>
