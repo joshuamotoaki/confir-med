40f676a51d49dffb3951b64dc989a8dcd42e5fd9
joshuamotoaki <dachshunduniverse@gmail.com>
2024-11-09 11:48:31 -0500

    Alert areas

--- apps/web/src/routes/(protected)/home/+page.svelte
+++ apps/web/src/routes/(protected)/home/+page.svelte
@@ -5,8 +5,42 @@
     import type { SupabaseClient } from "@supabase/supabase-js";
     import { getContext } from "svelte";
     import { toast } from "svelte-sonner";
+    import * as Alert from "$lib/components/ui/alert/";
+    import Warning from "$lib/icons/Warning.svelte";
+    import Info from "$lib/icons/Info.svelte";
+    import Close from "$lib/icons/Close.svelte";
+    import { fade } from "svelte/transition";
 
     const supabase = getContext<SupabaseClient>("supabase");
+
+    let warningsVisible = true;
+    let reportsVisible = true;
+
+    const warnings = [
+        {
+            id: 1,
+            patient: "Alice Margatroid",
+            message: "Forgot to take medication (acetaminophen) on 11/9."
+        },
+        {
+            id: 2,
+            patient: "Flandre Scarlet",
+            message: "Forgot to take medication (ibuprofen) on 11/9."
+        }
+    ];
+
+    const reports = [
+        {
+            id: 1,
+            patient: "Reimu Hakurei",
+            message: "Side effects of medication (acetaminophen) reported."
+        },
+        {
+            id: 2,
+            patient: "Marisa Kirisame",
+            message: "Side effects of medication (ibuprofen) reported."
+        }
+    ];
 </script>
 
 <div>
@@ -39,7 +73,82 @@
         </div>
     </header>
     <main>
-        <section id="alerts"></section>
-        <section id="patients"></section>
+        <div class="cont py-2">
+            <section id="alerts" class="space-y-2">
+                {#if warningsVisible}
+                    <div out:fade={{ duration: 300 }}>
+                        <Alert.Root variant="destructive">
+                            <button
+                                class="absolute right-2 top-2"
+                                aria-label="Close"
+                                onclick={() => {
+                                    warningsVisible = false;
+                                }}>
+                                <Close className="size-4 text-red-500" />
+                            </button>
+                            <Alert.Title>
+                                <div
+                                    class="justiyf-start flex items-center gap-1">
+                                    <Warning className="size-5" />
+                                    <h2 class=" font-semibold">Warnings</h2>
+                                </div>
+                            </Alert.Title>
+                            <Alert.Description>
+                                <div class="text-sm">
+                                    {#each warnings as warning}
+                                        <div>
+                                            <p>
+                                                {warning.patient}: {warning.message}
+                                            </p>
+                                        </div>
+                                    {/each}
+                                </div>
+                            </Alert.Description>
+                        </Alert.Root>
+                    </div>
+                {/if}
+
+                {#if reportsVisible}
+                    <div out:fade={{ duration: 300 }}>
+                        <Alert.Root>
+                            <button
+                                class="absolute right-2 top-2"
+                                aria-label="Close"
+                                onclick={() => {
+                                    reportsVisible = false;
+                                }}>
+                                <Close className="size-4 text-stone-500" />
+                            </button>
+
+                            <Alert.Title>
+                                <div
+                                    class="justiyf-start flex items-center gap-1">
+                                    <Info className="size-5" />
+                                    <h2 class="font-semibold">Reports</h2>
+                                </div>
+                            </Alert.Title>
+                            <Alert.Description variant="">
+                                <div class="text-sm">
+                                    {#each reports as report}
+                                        <div>
+                                            <p>
+                                                {report.patient}: {report.message}
+                                            </p>
+                                        </div>
+                                    {/each}
+                                </div>
+                            </Alert.Description>
+                        </Alert.Root>
+                    </div>
+                {/if}
+            </section>
+            <section id="patients"></section>
+        </div>
     </main>
 </div>
+
+<style lang="postcss">
+    article {
+        @apply rounded-md p-4 shadow-md;
+    }
+</style>
