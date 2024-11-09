cd93de9b5b51bb257725be225559e029d5242a3e
joshuamotoaki <dachshunduniverse@gmail.com>
2024-11-08 21:58:58 -0500

    Add supabase client

--- apps/web/src/lib/db.ts
+++
@@ -1,7 +0,0 @@
-import { DATABASE_URL } from "$env/static/private";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-
-// Disable prefetch as it is not supported for "Transaction" pool mode
-export const client = postgres(DATABASE_URL, { prepare: false });
-export const db = drizzle(client);
