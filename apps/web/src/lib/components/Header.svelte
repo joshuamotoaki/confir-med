ba8260df7dd5da4a87fe3920a7c9dc2fb7daaf33
joshuamotoaki <dachshunduniverse@gmail.com>
2024-11-09 17:32:43 -0500

    Add logo to header

--- apps/web/src/lib/components/Header.svelte
+++ apps/web/src/lib/components/Header.svelte
@@ -10,10 +10,13 @@
 
 <header class="std-flex h-16 bg-stone-100">
     <div class="cont btw-flex">
-        <div class="-space-y-2">
-            <h1 class="text-2xl font-bold text-blue-800">ConfirMed</h1>
-            <h2 class="text-sm text-stone-700">Doctor Portal</h2>
-        </div>
+        <a href="/home" class="flex items-center gap-2">
+            <img src="/logo.png" class="size-9" alt="Logo" />
+            <div class="-space-y-2">
+                <h1 class="text-2xl font-bold text-blue-800">ConfirMed</h1>
+                <h2 class="text-sm text-stone-700">Doctor Portal</h2>
+            </div>
+        </a>
         <nav class="std-flex gap-2">
             <Button variant="ghost" href="/home">
                 <span> Home </span>
