d0904a453d4168474e26d77bd1f049f4044f733f
joshuamotoaki <dachshunduniverse@gmail.com>
2024-11-09 11:20:02 -0500

    Basic header scaffolding

--- apps/web/src/routes/(protected)/home/+page.svelte
+++ apps/web/src/routes/(protected)/home/+page.svelte
@@ -1,1 +1,38 @@
-<div>Home</div>
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import Button from "$lib/components/ui/button/button.svelte";
+    import Plus from "$lib/icons/Plus.svelte";
+    import type { SupabaseClient } from "@supabase/supabase-js";
+    import { getContext } from "svelte";
+
+    const supabase = getContext<SupabaseClient>("supabase");
+</script>
+
+<div>
+    <header class="std-flex h-16 bg-stone-100">
+        <div class="cont btw-flex">
+            <div class="-space-y-2">
+                <h1 class="text-2xl font-bold text-blue-800">ConfirMed</h1>
+                <h2 class="text-sm text-stone-700">Doctor Portal</h2>
+            </div>
+            <nav class="std-flex gap-2">
+                <Button
+                    variant="ghost"
+                    onclick={async () => {
+                        const { error } = await supabase.auth.signOut();
+                        goto("/");
+                    }}>
+                    <span> Logout </span>
+                </Button>
+                <Button variant="outline">
+                    <Plus />
+                    <span> Add Patient </span>
+                </Button>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <section id="alerts"></section>
+        <section id="patients"></section>
+    </main>
+</div>
--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+    import { setContext } from "svelte";
     import "../app.css";
-    let { children } = $props();
+    let { data, children } = $props();
+    setContext("supabase", data.supabase);
 </script>
 
 {@render children()}
